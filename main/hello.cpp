2d62d054454b935535f1de4ff5a261180f1e36ee
sugnyan_sa7 <sugnyan_sa7@Ubuntu22.myguest.virtualbox.org>
2025-03-16 18:34:50 +0530

    Edited Hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Name: Sugnyan S Abbigeri\nSRN: PES1UG22CS622";
+  cout << "Name: Sugnyan S Abbigeri\nSRN: PES1UG22CS622\n";
+  cout << "Cloud Computing is my favourite subject!!!\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
