2db8b8a8d2a6d6c83131fea8a04b97170029f1b9
sugnyan_sa7 <sugnyan_sa7@Ubuntu22.myguest.virtualbox.org>
2025-03-16 19:25:37 +0530

    Added PES1UG22CS622.cpp

---
+++ main/PES1UG22CS622.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Your Name: Sugnyan S Abbigeri" << endl;
+    cout << "Your SRN: PES1UG22CS622" << endl;
+
+    for (int i = 1; i <= 5; i++) {
+        cout << i << endl;
+    }
+
+    return 0;
+}
+
